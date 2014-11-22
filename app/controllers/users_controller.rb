b4fb2614246e8fc160dee9cf5f93d22d38a4741b
won <wjin@nyit.edu>
2014-11-22 17:11:52 -0500

    modified users_contrller index new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def index
+
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:status] = TRUE
+      flash[:alert] = "You made it!"
+    else
+      flash[:status] = FALSE
+      flash[:alert] = @user.errors.full_messages
+    end
+    redirect_to register_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ActiveRecord::Base
+require 'digest/md5'
+  #attr_accessible :name, :password, :password_confirmation
+  before_save :encrypt_password
+
+  validates :name,
+    :presence => TRUE,
+    :length =>
+    {
+        :minimum => 4,
+        :allow_blank => TRUE
+    }
+
+  validates :password,
+    :presence => TRUE,
+    :length =>
+    {
+        :minimum => 6,
+        :allow_blank => TRUE
+    },
+    :confirmation => TRUE
+  validates :password_confirmation,
+    :presence => TRUE
+  def encrypt_password
+    self.password = Digest::MD5.hexdigest(password)
+  end
+
 end
